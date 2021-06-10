e8959b39bdf6f7d0ee1732d666ea1a955a316794
kmalski <Konrad.Malski@fis.agh.edu.pl>
2021-06-10 02:27:40 +0200

    Add video detection integration with GUI

--- app/app.py
+++ app/app.py
@@ -1,5 +1,5 @@
+from tkinter.constants import N
 from detection import ImageDetector, VideoDetector, Detector
-from utils import read_ImageTk
 from tkinter.filedialog import askopenfilename
 from tkinter import ttk
 import tkinter as tk
@@ -9,51 +9,80 @@ class App(tk.Tk):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.title('Video People Counter')
-        self.geometry('1024x768')
+        self.geometry('830x600')
         self.resizable(width=False, height=False)
         self.grid()
-        self.grid_rowconfigure(0, weight=1)
+        self.grid_rowconfigure(0, weight=10)
         self.grid_rowconfigure(1, weight=0)
+        self.grid_rowconfigure(2, weight=0)
         self.grid_columnconfigure(0, weight=1)
-
         self.add_widgets()
+        self.protocol('WM_DELETE_WINDOW', self.on_close)
 
         self._detector: Detector = None
 
     def add_widgets(self):
         self._panel = ttk.Label(master=self)
         self._panel.grid(row=0, column=0)
+        self._count_var = tk.StringVar()
+
+        counter_frame = ttk.Frame(master=self)
+        counter_frame.grid(row=1, column=0)
+        ttk.Label(counter_frame, text="Count:", font='Helvetica 12 bold')\
+            .grid(row=0, column=0)
+        ttk.Label(counter_frame, textvariable=self._count_var, font='Helvetica 12')\
+            .grid(row=0, column=1)
 
         button_frame = ttk.Frame(master=self)
-        button_frame.grid(row=1, column=0, pady=15)
+        button_frame.grid(row=2, column=0, pady=15)
         button_frame.grid_rowconfigure(0, weight=1)
         button_frame.grid_rowconfigure(1, weight=1)
         button_frame.grid_columnconfigure(0, weight=1)
         button_frame.grid_columnconfigure(1, weight=1)
 
         ttk.Button(master=button_frame, text='Select a video', width=70, command=self.load_video)\
-            .grid(row=0, column=0, padx=19, pady=20)
+            .grid(row=0, column=0, padx=21, pady=(0, 20))
         ttk.Button(master=button_frame, text='Select an image', width=70, command=self.load_image)\
-            .grid(row=0, column=1, padx=19, pady=20)
-        ttk.Button(master=button_frame, text='Detect people', width=148, command=self.start_detection)\
-            .grid(row=1, column=0, columnspan=2)
+            .grid(row=0, column=1, padx=21, pady=(0, 20))
+        ttk.Button(master=button_frame, text='Start detection', width=70, command=self.start_detection)\
+            .grid(row=1, column=0, padx=21)
+        ttk.Button(master=button_frame, text='Stop detection', width=70, command=self.stop_detection)\
+            .grid(row=1, column=1, padx=21)
 
     def load_video(self):
-        video_path = askopenfilename(filetypes=[('Video Files', '.mp4')])
+        video_path = askopenfilename(filetypes=[('Video Files', '.mp4 .mov .avi')])
         if video_path is not None and len(video_path) > 0:
+            self._count_var.set("")
             self._detector = VideoDetector(video_path)
+            frame = self._detector.get_tk_image()
+            self._panel.configure(image=frame)
+            self._panel.image = frame
 
     def load_image(self):
         image_path = askopenfilename(filetypes=[('Image Files', '.jpg .jpeg .png')])
         if image_path is not None and len(image_path) > 0:
-            image = read_ImageTk(image_path)
+            self._count_var.set("")
             self._detector = ImageDetector(image_path)
+            image = self._detector.get_tk_image()
             self._panel.configure(image=image)
             self._panel.image = image
 
     def start_detection(self):
+        if self._detector is not None and self._detector.stop:
+            self._detector.detect(self._panel, self._count_var)
+
+    def stop_detection(self):
         if self._detector is not None:
-            self._detector.detect(self._panel)
+            self._detector.stop = True
+
+    def on_close(self):
+        if self._detector is not None:
+            self._detector.stop = True
+            try:
+                self._detector.close()
+            except:
+                pass
+        self.destroy()
 
 
 if __name__ == '__main__':
--- app/detection.py
+++ app/detection.py
@@ -3,6 +3,7 @@ from abc import ABC, abstractmethod
 from imutils.object_detection import non_max_suppression
 from tkinter import ttk
 from PIL import Image, ImageTk
+import tkinter as tk
 import numpy as np
 import imutils
 import cv2
@@ -14,22 +15,25 @@ class Detector(ABC):
         self._path = path
         self._hog = cv2.HOGDescriptor()
         self._hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+        self.stop = True
 
     @abstractmethod
-    def detect(self, panel: ttk.Label) -> None:
+    def get_tk_image(self):
         pass
 
-    def single_frame_detect(self, image):
-        clone = image.copy()
+    @abstractmethod
+    def detect(self, panel: ttk.Label, text: tk.StringVar) -> None:
+        pass
 
+    def _image_to_tk_image(self, image):
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        image = Image.fromarray(image)
+        return ImageTk.PhotoImage(image)
+
+    def _single_frame_detect(self, image) -> list:
         (rects, _) = self._hog.detectMultiScale(image, winStride=(4, 4), padding=(8, 8), scale=1.05)
-
-        for (x, y, w, h) in rects:
-            cv2.rectangle(clone, (x, y), (x + w, y + h), (0, 0, 255), 2)
-
         rects = np.array([[x, y, x + w, y + h] for (x, y, w, h) in rects])
         result = non_max_suppression(rects, probs=None, overlapThresh=0.65)
-
         return result
 
 
@@ -37,45 +41,61 @@ class ImageDetector(Detector):
 
     def __init__(self, path: str):
         super().__init__(path)
+        self._image = cv2.imread(path)
+        self._image = imutils.resize(self._image, width=min(800, self._image.shape[1]))
 
-    def detect(self, panel: ttk.Label) -> None:
+    def get_tk_image(self):
+        return self._image_to_tk_image(self._image)
+
+    def detect(self, panel: ttk.Label, text: tk.StringVar) -> None:
         result = []
-        image = cv2.imread(self._path)
-        image = imutils.resize(image, width=min(800, image.shape[1]))
+        image = self._image.copy()
 
-        if len(image) <= 0:
-            return
-
-        result = self.single_frame_detect(image)
+        result = self._single_frame_detect(image)
 
         for (xA, yA, xB, yB) in result:
             cv2.rectangle(image, (xA, yA), (xB, yB), (0, 255, 0), 2)
 
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        image = Image.fromarray(image)
-        image = ImageTk.PhotoImage(image)
+        image = self._image_to_tk_image(image)
         panel.configure(image=image)
         panel.image = image
+        text.set(str(len(result)))
 
 
 class VideoDetector(Detector):
 
     def __init__(self, path: str):
         super().__init__(path)
+        self._cap = cv2.VideoCapture(path)
 
-    def detect(self, panel: ttk.Label) -> None:
-        cap = cv2.VideoCapture(self._path)
+    def get_tk_image(self):
+        _, frame = self._cap.read()
+        frame = imutils.resize(frame, width=min(800, frame.shape[1]))
+        self._cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
+        return self._image_to_tk_image(frame)
 
-        while cap.isOpened():
-            ret, frame = cap.read()
+    def detect(self, panel: ttk.Label, text: tk.StringVar, force=True) -> None:
+        if force:
+            self.stop = False
 
-            if not ret:
-                break
+        if not self.stop and self._cap is not None and self._cap.isOpened:
+            ok, frame = self._cap.read()
+        else:
+            return
 
-            frame = imutils.resize(frame, width=min(400, frame.shape[1]))
-            result = self.single_frame_detect(frame.copy())
-
+        if ok:
+            frame = imutils.resize(frame, width=min(800, frame.shape[1]))
+            result = self._single_frame_detect(frame.copy())
             for (xA, yA, xB, yB) in result:
-                cv2.rectangle(frame, (xA, yA), (xB, yB), (0, 255, 0), 2)
+                cv2.rectangle(frame, (xA, yA), (xB, yB), (0, 255, 0), 1)
+            frame = self._image_to_tk_image(frame)
 
-        # TODO: add frame to passed panel arg
+            panel.configure(image=frame)
+            panel.image = frame
+            text.set(str(len(result)))
+
+        panel.after(15, lambda: self.detect(panel, text, force=False))
+
+    def close(self):
+        if self._cap is not None and self._cap.isOpened():
+            self._cap.release()
